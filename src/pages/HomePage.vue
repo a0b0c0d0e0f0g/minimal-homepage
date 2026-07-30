cf8ebff86c4dbcd5be88dcbc3d4f73ee0a947a41
a0b0c0d0e0f0g <a2034574778@outlook.com>
2026-07-30 23:27:44 +0800

    Update HomePage.vue

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,6 +10,6 @@
 <script setup>
 import HeroSection from '../components/HeroSection.vue'
 import CardSection from '../components/CardSection.vue'
-import MyProject from '../components/MyProject.vue'
+//import MyProject from '../components/MyProject.vue'
 import RSSSection from '../components/RSSSection.vue'
 </script>
